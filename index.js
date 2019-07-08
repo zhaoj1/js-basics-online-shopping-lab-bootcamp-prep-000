058863a753e11da6bcbd7ac45176905a307d9b29
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-07-08 08:38:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ function removeFromCart(item) {
       return cart
     }
   }
-  console.log("That item is not in your cart.")
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 
