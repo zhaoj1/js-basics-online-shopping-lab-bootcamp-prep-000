53487d2b881571bfaac713cbeaf957c36684ff0d
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-07-08 08:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,21 +9,53 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ var itemPrice = (Math.floor(Math.random()*100)+1);
+ var obj = { "itemName" : itemName, "itemPrice":itemPrice };
+ cart.push(obj);
+ return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var str = []
+  if(cart.length===0){
+    return "Your shopping cart is empty.";
+  }
+  else if(cart.length===1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  else if(cart.length===2){
+    for(let i=0; i<cart.length;i++){
+      str.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+    return "In your cart, you have " + str.join(", and ")+"."
+  }
+  else{
+    for(let i = 0; i<cart.length-1; i++){
+      str.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+      str.push(`and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}`)
+    return "In your cart, you have " + str.join(", ")+".";
+  }
 }
 
 function total() {
   // write your code here
+  var price = 0;
+  for(let i=0;i<cart.length;i++){
+    price += cart[i].itemPrice
+  }
+  return price
 }
 
 function removeFromCart(item) {
   // write your code here
+  console.log(cart.includes(item))
 }
+
+
 
 function placeOrder(cardNumber) {
   // write your code here
