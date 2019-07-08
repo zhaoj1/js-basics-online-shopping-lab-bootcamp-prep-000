8fa608f76944dfe4f06d1ceb4700f41319e145c4
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-07-08 08:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here"
   for(let i = 0; i < cart.length; i++){
-    if(){
+    if(cart.has){
       cart = [...cart.splice(0,i),...cart.splice(i+1)];
       return cart;
     }
