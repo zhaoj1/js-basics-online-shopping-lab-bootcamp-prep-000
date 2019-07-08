dad506ccd558556663231e67e80955f951dc77f4
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-07-08 08:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ function total() {
 function removeFromCart(item) {
   // write your code here"
   for(let i = 0; i<cart.length;i++){
-    if(cart[i].)
+    if(cart[i].itemName ===item){
+      
+    }
   }
 }
 
